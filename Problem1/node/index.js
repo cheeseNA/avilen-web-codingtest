1c401a50c8e49dc00ea688a6368e7a388fc8e8b9
cheeseNA <kurocat2000@gmail.com>
2022-05-29 15:16:33 +0900

    Update Problem1/node/index.js to contain query strings

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -2,10 +2,18 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res)=>{
-   const queryObject = url.parse(req.url, true).query;
-   console.log(queryObject.obj);
+   // const queryObject = url.parse(req.url, true).query;
+   // console.log(queryObject.obj);
 
    // ここに処理を記述してください。
+   const queryObject = {
+      "obj": [
+         { "num": 4, "text": "fizz" },
+         { "num": 7, "text": "buzz" },
+         { "num": 8, "text": "hoge" },
+         { "num": 15, "text": "huga" }
+      ]
+   };
    ans = "answer";
 
    res.setHeader('Access-Control-Allow-Origin', '*');
